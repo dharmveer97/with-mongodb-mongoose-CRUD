829dfe0001d96942360210a7bf2b5202adb2b6e5
dharmveer97 <dvbangar8@gmail.com>
2022-10-24 19:20:12 +0530

    backend created

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 // /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    MONGODB_URI:
-      'mongodb+srv://dharmveer97:p9Hyui3PRD6njXtM@cluster0.5yvdkhl.mongodb.net/?retryWrites=true&w=majority',
-  },
+  // env: {
+  //   MONGODB_URI:
+  //     '',
+  // },
   reactStrictMode: true,
   swcMinify: true,
 };
