aa753a3de3f8a42aec3baaddb24f6fe0b4f7d334
dharmveer97 <dvbangar8@gmail.com>
2022-10-25 12:47:10 +0530

    dummy data added with map

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,17 @@
 import { Grid } from '@nextui-org/react';
 import CardItem from './elements/CardItem';
 
+const data = [{ title: 'First item' }, { title: 'Second item' }, { title: 'Third item' }];
+
 function Hero() {
   return (
     <Grid.Container gap={2} justify="center">
-      <Grid xs={4}>
-        <CardItem text="1 of 3">Hello</CardItem>
-      </Grid>
-      <Grid xs={4}>
-        <CardItem text="1 of 3">Hello</CardItem>
-      </Grid>
-      <Grid xs={4}>
-        <CardItem text="1 of 3">Hello</CardItem>
-      </Grid>
+      {data?.map((element) => (
+        <Grid xs={4}>
+          <CardItem>{element.title}</CardItem>
+        </Grid>
+      ))}
+
     </Grid.Container>
   );
 }
