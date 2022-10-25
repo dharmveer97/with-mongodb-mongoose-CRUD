3d9ab78e3569a3bf5c4de6ac30c8de43e33a9c9a
dharmveer97 <dvbangar8@gmail.com>
2022-10-25 14:32:49 +0530

    minor text change and button fixed

--- src/components/elements/CardItem.js
+++ src/components/elements/CardItem.js
@@ -1,5 +1,5 @@
 import {
-  Card, Text, Row,
+  Card, Text, Row, Link,
 } from '@nextui-org/react';
 
 import Button from './Button';
@@ -21,14 +21,19 @@ function CardItem({ data }) {
       <Card.Body css={{ py: '$10' }}>
         <Text>
           {data.realName}
+          {' '}
           | No alter egos found || Justice League Of America, Batman Family
         </Text>
       </Card.Body>
       <Card.Divider />
       <Card.Footer>
+
         <Row justify="flex-end">
-          <Button size="sm">
-            Learn more
+          <Button color="primary" auto ghost as={Link} size="sm" href="/" css={{ marginRight: '10px' }}>
+            Edit Hero
+          </Button>
+          <Button as={Link} size="sm" href="/">
+            View Hero
           </Button>
         </Row>
       </Card.Footer>
