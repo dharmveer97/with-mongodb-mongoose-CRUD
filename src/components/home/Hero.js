da813cd19434b0df1e6662a6b854348c7af5f841
dharmveer97 <dvbangar8@gmail.com>
2022-10-25 14:10:06 +0530

    minor fix

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,5 +1,5 @@
 import { Grid } from '@nextui-org/react';
-import CardItem from './elements/CardItem';
+import CardItem from '../elements/CardItem';
 
 const data = [{ title: 'First item' }, { title: 'Second item' }, { title: 'Third item' }];
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Layout from '../components/Layout';
-import Hero from '../components/Hero';
+import Hero from '../components/home/Hero';
 
 function Index() {
   return (
